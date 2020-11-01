1c6599c4c2b41ea7413028fb87b3d5304a17fd44
lffavaretto <luizfelipefavaretto21@gmail.com>
2020-11-01 17:34:57 -0300

    sim

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const express = require('express')
-
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("requireDir");
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/nodeapi",{userNewUrlParser: true});
+
+requireDir('./src/models/product');
+
+const product = mongoose.model('Product');
+app.get('/',(req, res) => {
+    product.create({
+        title: 'Rwact Native',
+        description:'Build native apps with React',
+        url:'http://github.com/facebook/react-native'
+    })
+ return res.send('Hello rockseat')
+});
 
 app.listen(3001);
